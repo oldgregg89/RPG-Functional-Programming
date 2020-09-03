f55fc5e84611f395c14aba1dfaf38878dfed9bc5
Ian Gregg <iangregg188@gmail.com>
2020-09-03 10:11:49 -0700

    have most of the testing working for the inventory.
    Co-Authored-By: Chris Yoon <chy.yoon@gmail.com>
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- _tests_/inventory.test.js
+++ _tests_/inventory.test.js
@@ -1,5 +1,17 @@
-import {item, magicArmor, bowstaff, lifeOrb, armorSpecific, attackSpecific, lifeSpecific, itemsStats} from '../src/inventory.js';
+import {item, magicArmor, bowstaff, lifeOrb, itemsStats} from '../src/inventory.js';
 
 test('it should create a test item', () => {
   expect(item).toMatchObject({life: 0, armor:0, strike: 0})
 });
+
+test('bowstaff should have an attack of 1', () => {
+  let item = {life: 0, armor:0, strike: 0}
+  expect(bowstaff).toMatchObject({life: 0, armor:0, strike: 2})
+});
+
+test('magicArmor should have 2 life & 2 armor', () => {
+  let item = {life: 0, armor:0, strike: 0}
+  expect(magicArmor).toMatchObject({life: 2, armor:2, strike:0})
+});
+
+test('lifeOrb should have ')
